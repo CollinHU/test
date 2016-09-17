416e31a416b68c23402f8e48113a8e6216cf516d
CollinHU <collinhu@link.cuhk.edu.hk>
2016-09-17 11:53:55 +0800

    add input function

--- MyMergeSort.java
+++ MyMergeSort.java
@@ -1,66 +1,87 @@
-/*package com.java2novice.sorting;*/
+import java.util.Scanner;
+public class MergeSort
+{
+  private int[] array;
+  private int[] tempArray;
+  private int length;
 
-public class MyMergeSort {
+  public static void main(String a[])
+  {
+    Scanner input = new Scanner(System.in);
+    int num;
+    System.out.printf("please type in the number: ");
+    num = input.nextInt();
 
-    private int[] array;
-    private int[] tempMergArr;
-    private int length;
+    int[] inputArray = new int[num];
 
-    public static void main(String a[]){
-
-        int[] inputArr = {45,23,11,89,77,98,4,28,65,43};
-        MyMergeSort mms = new MyMergeSort();
-        mms.sort(inputArr);
-        for(int i:inputArr){
-            System.out.print(i);
-            System.out.print(" ");
-        }
+   for(int i = 0;i < inputArray.length;i++)
+    {
+      System.out.printf("please %d-th interge :",i+1);
+      inputArray[i] = input.nextInt();
     }
 
-    public void sort(int inputArr[]) {
-        this.array = inputArr;
-        this.length = inputArr.length;
-        this.tempMergArr = new int[length];
-        doMergeSort(0, length - 1);
+    MergeSort objA= new MergeSort();
+    objA.sort(inputArray);
+
+    for(int i:inputArray)
+    {
+      System.out.print(i);
+      System.out.print(" ");
+    }
+    System.out.println(" ");
+
+  }
+
+  public void sort(int inputArray[])
+  {
+    this.array = inputArray;
+    this.length = inputArray.length;
+    this.tempArray = new int[length];
+    doMergeSort(0,length-1);
+  }
+
+  private void doMergeSort(int lowIndex, int highIndex)
+  {
+    if(lowIndex < highIndex)
+    {
+      int middle = lowIndex + (highIndex - lowIndex)/2;
+
+      doMergeSort(lowIndex,middle);
+      doMergeSort(middle+1,highIndex);
+      merge(lowIndex,middle,highIndex);
+    }
+  }
+
+  private void merge(int lowIndex,int middle,int highIndex)
+  {
+    for(int i = lowIndex;i <= highIndex;i++)
+    {
+      tempArray[i] = array[i];
     }
 
-    private void doMergeSort(int lowerIndex, int higherIndex) {
+    int i = lowIndex;
+    int j = middle + 1;
+    int k = lowIndex;
 
-        if (lowerIndex < higherIndex) {
-            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
-            // Below step sorts the left side of the array
-            doMergeSort(lowerIndex, middle);
-            // Below step sorts the right side of the array
-            doMergeSort(middle + 1, higherIndex);
-            // Now merge both sides
-            mergeParts(lowerIndex, middle, higherIndex);
-        }
+    while(i <= middle && j <= highIndex)
+    {
+      if(tempArray[i] < tempArray[j])
+      {
+        array[k] = tempArray[i];
+        i++;
+      }
+      else
+      {
+        array[k] = tempArray[j];
+        j++;
+      }
+      k++;
     }
-
-    private void mergeParts(int lowerIndex, int middle, int higherIndex) {
-
-        for (int i = lowerIndex; i <= higherIndex; i++) {
-            tempMergArr[i] = array[i];
-        }
-        int i = lowerIndex;
-        int j = middle + 1;
-        int k = lowerIndex;
-        while (i <= middle && j <= higherIndex) {
-            if (tempMergArr[i] <= tempMergArr[j]) {
-                array[k] = tempMergArr[i];
-                i++;
-            } else {
-                array[k] = tempMergArr[j];
-                j++;
-            }
-            k++;
-        }
-        while (i <= middle) {
-            array[k] = tempMergArr[i];
-            k++;
-            i++;
-        }
-
+    while(i <= middle)
+    {
+      array[k] = tempArray[i];
+      i++;
+      k++;
     }
+  }
 }
-/*See more at: http://www.java2novice.com/java-sorting-algorithms/merge-sort/#sthash.hphFSzwd.dpuf*/
